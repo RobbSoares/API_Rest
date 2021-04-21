dc5af3d44c408039334e527d5649a3f4b8f105d5
Robson Soares <robson.santana.soares@gmail.com>
2021-04-21 11:21:20 -0300

    Adicionando arquivos

---
+++ src/database/seeds/20210419193427-criar-usuarios.js
@@ -0,0 +1,33 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Carlos',
+        email: 'testing@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'José',
+        email: 'testing1@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Romulo',
+        email: 'testing2@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: async () => {},
+};
