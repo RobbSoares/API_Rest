d3d8879cfb16e4363b0288b6ff0c6023e921a0ec
Robson Soares <robson.santana.soares@gmail.com>
2021-04-24 19:45:00 -0300

    -

--- dist/app.js
+++ dist/app.js
@@ -41,7 +41,7 @@ class App {
 
   middlewares() {
     this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       next();
     });
